4f4f65d330b37a1950ffa42a1a50aa52df7414fa
Tayuki1538 <stucbk953-mmm@g.ecc.u-tokyo.ac.jp>
2022-11-10 22:23:27 +0900

    市町村名、丁目を追加

--- onsen_info.py
+++ onsen_info.py
@@ -2,7 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 import pandas
 import time
-
+import re
+from urllib import request, parse
+import numpy as np
 
 
 def get_items(url, df, columns):
@@ -14,6 +16,9 @@ def get_items(url, df, columns):
     for item in ret:
         prefecture = item.select("td.kn > a")[0].text
         name = item.select("td.sn > a")[0].text
+        onsen_url = item.select("td.sn")[0].find("a").get("href")
+        latitude, longitude = get_latlong(onsen_url)
+        city, lv01Nm = reverse_geocoding(latitude, longitude)
         try:
             links = item.select("td.hp")[0]
             link = links.find("a").get("href")
@@ -64,15 +69,60 @@ def get_items(url, df, columns):
             kokyu = "不明"
 
         print("{0}番目の情報:{1}をDataFrameに追加します...".format(len(df)+1, name))
-        print("県名:{0} 温泉名:{1} リンク:{2} 効能:{3} 評価平均:{4} 口コミ:{5} 宿数:{6} 温泉数:{7} 大浴場:{8} 露天風呂:{9} サウナ:{10} 禁煙:{11} 高級:{12}" \
-            .format(prefecture, name, link, kounou, hyoka, comi, yado, onsen_num, dai, roten, sauna, kinen, kokyu))
-        se = pandas.Series([prefecture, name, link, kounou, hyoka, comi, yado, onsen_num, dai, roten, sauna, kinen, kokyu], columns)
+        print("県名:{0} 温泉名:{1} リンク:{2} 効能:{3} 評価平均:{4} 口コミ:{5} 宿数:{6} 温泉数:{7} 大浴場:{8} 露天風呂:{9} サウナ:{10} 禁煙:{11} 高級:{12} 市町村:{13} 丁名:{14}" \
+            .format(prefecture, name, link, kounou, hyoka, comi, yado, onsen_num, dai, roten, sauna, kinen, kokyu, city, lv01Nm))
+        se = pandas.Series([prefecture, name, link, kounou, hyoka, comi, yado, onsen_num, dai, roten, sauna, kinen, kokyu, city, lv01Nm], columns)
         df = df.append(se, ignore_index = True)
     return df
 
+def get_latlong(url):
+    url_re = "https://yadococo.net/" + url
+    res = requests.get(url_re)
+    res_text = res.text
+    soup = BeautifulSoup(res_text, 'html.parser')
+    ret = soup.find_all("tr")
+    ret.pop(0)
+    ret.pop(-1)
+    lat_list = []
+    long_list = []
+    for item in ret:
+        try:
+            links = item.select("td.mp")[0]
+            link = links.find("a").get("href")
+        except IndexError:
+            print("Error\n")
+        list_mp = link.split("&")
+        try:
+            lalo = list_mp[1].split("=")
+            latitude, longitude = lalo[1].split(",")
+            lat_list.append(float(latitude))
+            long_list.append(float(longitude))
+        except:
+            pass
+    time.sleep(1)
+    if len(lat_list) == 0:
+        return [0, 0]
+    else:
+        return np.mean(lat_list), np.mean(long_list)
+
+def reverse_geocoding(latitude, longitude):
+    endpoint_url = 'https://mreversegeocoder.gsi.go.jp/reverse-geocoder/LonLatToAddress'
+    params = {
+        'lat':latitude,
+        'lon':longitude
+    }
+    res = requests.get(endpoint_url, params=params)
+    data = res.json()
+    d = data["results"]
+    muniCd = d["muniCd"]
+    lv01Nm = d["lv01Nm"]
+    df = pandas.read_csv("municode.csv").astype({'muniCd':str})
+    city = df.loc[df["muniCd"] == str(int(muniCd)), "chiriin_city_name"].iloc[-1]
+    return city, lv01Nm
+
 def main():
     url = "https://yadococo.net/onsen.php"
-    columns = ["prefecture", "name", "link", "kounou", "hyoka", "comi", "yado", "onsen_num", "dai", "roten", "sauna", "kinen", "kokyu"]
+    columns = ["prefecture", "name", "link", "kounou", "hyoka", "comi", "yado", "onsen_num", "dai", "roten", "sauna", "kinen", "kokyu", "city", "lv01Nm"]
     df = pandas.DataFrame(columns=columns)
     results = get_items(url, df, columns)
     results.to_csv("onsen_info.csv", index= False)
